6b29ded8100376797b752dbdba94c942fab8742c
Wenke-Liu <wenke.liu1986@gmail.com>
2020-04-24 15:14:25 -0400

    04-24-20

--- layers.py
+++ layers.py
@@ -1,17 +1,16 @@
 import tensorflow as tf
-import keras
 
 
-class DenseTranspose(keras.layers.Layer):
+class DenseTranspose(tf.keras.layers.Layer):
 
     def __init__(self, dense, activation=None, use_bias=False, **kwargs):
         self.dense = dense
         self.use_bias = use_bias
+        self.activation = tf.keras.activations.get(activation)
+        super().__init__(**kwargs)
         self.biases = self.add_weight(name='bias',
                                       shape=[self.dense.input_shape[-1]],
                                       initializer='zeros')
-        self.activation = keras.activations.get(activation)
-        super().__init__(**kwargs)
 
     def build(self, batch_input_shape):
         super().build(batch_input_shape)
@@ -22,34 +21,3 @@ class DenseTranspose(keras.layers.Layer):
             z = z + self.biases
         return z
 
-
-class Encoder(keras.layers.Layer):
-    def __init__(self,
-                 hidden_size,
-                 latent_size,
-                 activation,
-                 name='encoder',
-                 **kwargs):
-        super(Encoder, self).__init__(name=name, **kwargs)
-        self.hidden = keras.layers.Dense(hidden_size, activation=activation)
-        self.latent = keras.layers.Dense(latent_size, activation='linear')
-
-    def call(self, inputs, **kwargs):
-        x = self.hidden(inputs)
-        return self.latent(x)
-
-
-class Decoder(keras.layers.Layer):
-    def __init__(self,
-                 hidden_size,
-                 output_size,
-                 activation,
-                 name='decoder',
-                 **kwargs):
-        super(Decoder, self).__init__(name=name, **kwargs)
-        self.hidden = keras.layers.Dense(hidden_size, acitvation=activation)
-        self.reconstructed = keras.layers.Dense(output_size, activation='linear')
-
-    def call(self, latents, **kwargs):
-        x = self.hidden(latents)
-        return self.reconstructed(x)
--- models.py
+++ models.py
@@ -1,11 +1,11 @@
 from datetime import datetime
+from collections.abc import Iterable
 import os
 import re
 import sys
 
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
 import layers
 
 
@@ -23,6 +23,7 @@ class ICANN:
                  hidden_size=None,
                  latent_size=None,
                  nonlinearity='relu',
+                 ica_weight=0.005,
                  learning_rate=1e-3,
                  batch_size=256,
                  model_dir=None):
@@ -31,53 +32,61 @@ class ICANN:
         self.hidden_size = hidden_size
         self.latent_size = latent_size
         self.nonlinearity = nonlinearity
+        self.ica_weight = ica_weight
         self.learning_rate = learning_rate
         self.batch_size = batch_size
 
-        if model_dir:
-            pass  # under construction: update architecture from model name, load saved weights to built model
-
         self.model = self.build()
 
-    def build(self):
-        inputs = keras.Input(shape=(self.input_size,), name='encoder_input')
+        if model_dir:  # Built model architecture and load saved weights from file
+            self.model.load_weights(model_dir)
 
-        if not self.hidden_size:  # linear RICA
-            encoder = keras.layers.Dense(self.latent_size,
-                                         activation='linear',
-                                         use_bias=False,
-                                         name='encoder')
+    def build(self):
+        inputs = tf.keras.Input(shape=(self.input_size,), name='input')
+
+        if not self.hidden_size:  # linear RICA, tied weights
+            encoder = tf.keras.layers.Dense(self.latent_size,
+                                          activation='linear',
+                                          use_bias=False,
+                                          name='encoder')
+            latents = encoder(inputs)
             decoder = layers.DenseTranspose(encoder,
                                             activation='linear',
                                             use_bias=False,
                                             name='decoder')
-            latents = encoder(inputs)
-            reconstructed = decoder(latents, use_bias=False)
-        else:  # nonlinear version, with one hidden layer
-            encoder = layers.Encoder(hidden_size=self.hidden_size,
-                                     latent_size=self.latent_size,
-                                     activation=self.nonlinearity,
-                                     name='encoder')
-            decoder = layers.Decoder(hidden_size=self.hidden_size,
-                                     output_size=self.input_size,
-                                     activation=self.nonlinearity,
-                                     name='decoder')
-            latents = encoder(inputs)
             reconstructed = decoder(latents)
+        else:  # nonlinear version, with hidden layers, not tying weights
+            h_encoded = inputs
+            if isinstance(self.hidden_size, Iterable):
+                h_list = list(self.hidden_size)
+            else:
+                h_list = [self.hidden_size]
 
-        model = keras.Model(inputs=inputs, outputs=reconstructed, name='ae')
+            for idx, h in enumerate(h_list):
+                h_encoded = tf.keras.layers.Dense(h, activation=self.nonlinearity,
+                                                  name='encoder_'+str(idx))(h_encoded)
+
+            latents = tf.keras.layers.Dense(self.latent_size, activation='linear', name='latent')(h_encoded)
+            h_encoded = latents
+            for idx, h in enumerate(reversed(h_list)):
+                h_encoded = tf.keras.layers.Dense(h, activation=self.nonlinearity,
+                                                  name='decoder_'+str(idx))(h_encoded)
+
+            reconstructed = tf.keras.layers.Dense(self.input_size, activation='linear', name='output')(h_encoded)
+
+        model = tf.keras.Model(inputs=inputs, outputs=reconstructed, name='ae')
         sq = tf.multiply(latents, latents)
         kurtosis = tf.reduce_mean(tf.multiply(sq, sq), axis=0)
-        loss_ica = tf.reduce_mean(kurtosis)
+        loss_ica = tf.reduce_mean(kurtosis)*self.ica_weight
         model.add_loss(loss_ica)
-        optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
-        model.compile(optimizer, loss=tf.keras.losses.mean_squared_error())
+        optimizer = tf.keras.optimizers.Adam(lr=self.learning_rate)
+        model.compile(optimizer, loss=tf.keras.losses.mean_squared_error)
 
         return model
 
     def train(self, x_train, epochs=10, save=True, model_dir='./model', log_dir='./log'):
         self.model.fit(x_train, x_train,
-                       batch_size=self.batch_size,
+                       batch_size=self.batch_size, shuffle=True,
                        epochs=epochs)
         if save:
             self.model.save_weights(model_dir)
@@ -85,7 +94,7 @@ class ICANN:
     def reconstructed(self, inputs):
         return self.model(inputs)
 
-    def inference(self, inputs):
-        latents_out = self.model.get_layer("encoder").output
-        inference_model = keras.Model(inputs=self.model.input, outputs=latents_out)
-        return inference_model(inputs)
+    def inference(self, x):
+        latents_out = self.model.get_layer("latent").output
+        inference_model = tf.keras.Model(inputs=self.model.input, outputs=latents_out)
+        return inference_model.predict(x)
